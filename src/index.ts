7817b65f651cb1ec02e19ccade16858962b82831
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-02-04 15:21:27 +0200

    adding mongo & mongoose & starting User model

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+
 
 
 const app = express();
@@ -21,3 +23,7 @@ server.listen(8080, () => {
     console.log(`Server running on http://localhost:8080`);
     
 })
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
